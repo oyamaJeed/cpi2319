591242bcc184b5b7fb62b07610e43df4de9fe196
nisikawa <meit2319@kinki-pc.ac.jp>
2023-04-25 03:35:24 -0700

    [Add]README.md.

---
+++ test2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+extern char *dsp(int, char *);
+
+
+int main(int argc, char *argv[])
+{
+  for (int i = 0; i < argc; i++) {
+    dsp(i, argv[i]);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
